4adebd21719e44529c2751c5cd3cf51f8803f8fd
maryvi <mcamachog@gmail.com>
2024-03-19 19:18:22 +0000

    solucion original

--- usuario.py
+++ usuario.py
@@ -6,14 +6,28 @@ class Usuario():
         self.genero = genero
 
 import json
+import os
+from datetime import datetime
+
 instancias = []
 
 with open("usuarios.txt") as usuarios:
     linea = usuarios.readline()
     while linea:
-        usuario = json.loads(linea)
-        instancias.append(Usuario(usuario.get("nombre"), usuario.get("apellido"),usuario.get("email"), usuario.get("genero"))) 
-        linea = usuarios.readline()
+        try:
+            usuario = json.loads(linea)
+            instancias.append(Usuario(usuario.get("nombre"), usuario.get("apellido"),usuario.get("email"), usuario.get("genero"))) 
+            linea = usuarios.readline()
+        except Exception as e:
+            path = os.path.join("logs", "error.log")
+            with open(path, "a+") as log:
+                log.seek(0)
+                now = datetime.now()
+                log.write(f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] ERROR: {e}\n")
+            break
 
 for usuario in instancias:
     print(f"Nombre del usuario: {usuario.nombre}, Apellido: {usuario.apellido}, Email: {usuario.email}, Genero: {usuario.genero} ") 
+
+
+
